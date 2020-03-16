6d3771a9f8186a25af8fd43f1337de491fcbbfbd
Nathan Yang <nathanyang@engineering.ucla.edu>
2020-03-15 20:40:14 -0700

    Updated react-scripts package

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>exploretech.la</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>This is in the website repo.</p>
+        <p>This is in the <a href="https://github.com/exploretech-la/website" target="_blank">website</a> repo.</p>
       </div>
     );
   }
