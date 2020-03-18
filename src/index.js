7a9590ffde4cf1b417049468da978bb15ce25c2e
Nathan Yang <nathanyang@engineering.ucla.edu>
2020-03-17 23:13:07 -0700

    Added Bootstrap CSS and Poppins font

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
-import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.scss';
 
 ReactDOM.render(
   <App />,
