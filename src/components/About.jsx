baa1d217f41b0df754eb11e25e7c653983807439
Nathan Yang <nathanyang@engineering.ucla.edu>
2020-05-23 16:44:14 -0700

    Minor refactoring

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export default class About extends Component {
                     <h3 className="section-title">What is exploretech.la?</h3>
                     <div className="about-content">
                         <p>exploretech.la is an annual event held at UCLA that aims to inspire high school students from underserved communities in the Greater Los Angeles Area to explore computer science, engineering, and technology.</p>
-                        <p>Through our panels, interactive workshops, exhibitor booths, and partnerships with industry and academic organizations, we introduce students to the larger applications tech outside the classroom.</p>
+                        <p>Through our panels, interactive workshops, exhibitor booths, and partnerships with industry and academic organizations, we introduce students to the larger applications of tech outside the classroom.</p>
                     </div>
                 </div>
                 <img src={MicrosoftVRDemo} className="workshop-photo" alt="workshop-photo" />
