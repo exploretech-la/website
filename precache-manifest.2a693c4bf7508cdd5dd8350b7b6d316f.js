aedb21f011647667fefb1093f200bc6fd145d2f2
Nathan Yang <nathanyang@engineering.ucla.edu>
2020-09-19 17:28:36 -0700

    Updates

---
+++ precache-manifest.2a693c4bf7508cdd5dd8350b7b6d316f.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763a462e7aa6fcd4d02a5c31c375a1df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb3e4bedb45d13fb61fc",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "e95ba21a3acd8575d133",
+    "url": "/static/css/main.408e7a50.chunk.css"
+  },
+  {
+    "revision": "bb3e4bedb45d13fb61fc",
+    "url": "/static/js/2.14db8716.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.14db8716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95ba21a3acd8575d133",
+    "url": "/static/js/main.d6c0f769.chunk.js"
+  },
+  {
+    "revision": "068310cf975a011c792f",
+    "url": "/static/js/runtime-main.026b2492.js"
+  },
+  {
+    "revision": "6abbf0ed0ada77658d702bdbf5896c0e",
+    "url": "/static/media/capcom.6abbf0ed.svg"
+  },
+  {
+    "revision": "59523cba815f66fbe9544491b38ec933",
+    "url": "/static/media/carey-nachenberg.59523cba.png"
+  },
+  {
+    "revision": "3b2da79efd0b4589af4af549dd200b6a",
+    "url": "/static/media/coca-cola.3b2da79e.svg"
+  },
+  {
+    "revision": "a19c84474e5db1b55eb3d6ad99271a8c",
+    "url": "/static/media/demo-microsoft-vr.a19c8447.jpg"
+  },
+  {
+    "revision": "9837f38a0bdd7a352fb766ae54625e25",
+    "url": "/static/media/google.9837f38a.png"
+  },
+  {
+    "revision": "6111a0dc58c96d5e31e039decd6380f1",
+    "url": "/static/media/jayathi-murthy.6111a0dc.png"
+  },
+  {
+    "revision": "8ab7e5ebf44e953596d3b1ff6f7c2060",
+    "url": "/static/media/justin-brezhnev.8ab7e5eb.png"
+  },
+  {
+    "revision": "de4e45c9d7098d703e8c1bce59643b66",
+    "url": "/static/media/linode.de4e45c9.svg"
+  },
+  {
+    "revision": "0df0021fc8325f9c4efa65d6e1bcf8e6",
+    "url": "/static/media/logo-compass.0df0021f.svg"
+  },
+  {
+    "revision": "8e72826863b0a150c77b4463689371a1",
+    "url": "/static/media/logo-navy-with-icon-group-white.8e728268.svg"
+  },
+  {
+    "revision": "b86757b23a54e88618f16511d06ae401",
+    "url": "/static/media/logo-with-icons-navy.b86757b2.svg"
+  },
+  {
+    "revision": "3aea7cbcfe713aebe2f75be0b2b12f74",
+    "url": "/static/media/logo-with-icons-white.3aea7cbc.svg"
+  },
+  {
+    "revision": "549b34636c9d3ee44c6045f9f9ffcc60",
+    "url": "/static/media/microsoft.549b3463.png"
+  },
+  {
+    "revision": "1cf19b81367144cfb3f1dfd46fa86af4",
+    "url": "/static/media/nick-corral.1cf19b81.jpg"
+  },
+  {
+    "revision": "28a461fac1579036ad536a4e7f0ae835",
+    "url": "/static/media/northrop-grumman.28a461fa.png"
+  },
+  {
+    "revision": "8a284de7845039414194e40dc5366704",
+    "url": "/static/media/ruth-johnson.8a284de7.jpeg"
+  },
+  {
+    "revision": "e6f61a67b7da2af30e06f3fffd8d4577",
+    "url": "/static/media/solomon-russell.e6f61a67.jpeg"
+  },
+  {
+    "revision": "a2e04b80dd0c054095001c8858c10673",
+    "url": "/static/media/speaker-connie.a2e04b80.jpg"
+  },
+  {
+    "revision": "530245ac3fb05ebe8e9872112535330e",
+    "url": "/static/media/taboola-booth.530245ac.jpg"
+  },
+  {
+    "revision": "61658ddae191c3ca1c25995a824c0453",
+    "url": "/static/media/teals-vr.61658dda.jpg"
+  },
+  {
+    "revision": "b0edb22bab632acd1d0a61a8c6a279bc",
+    "url": "/static/media/teals.b0edb22b.png"
+  },
+  {
+    "revision": "adea72bd5f6ddc1807873ddf206bf294",
+    "url": "/static/media/team-photo-2020.adea72bd.jpg"
+  },
+  {
+    "revision": "665983d47832ba9cc7d2be0a5fa29326",
+    "url": "/static/media/tyler-menezes.665983d4.png"
+  },
+  {
+    "revision": "0986e1c59418f943b41d055021c87237",
+    "url": "/static/media/ucla-cs.0986e1c5.png"
+  },
+  {
+    "revision": "a1880337ae61ab6852992ab1ad776d6b",
+    "url": "/static/media/ucla-samueli-engineering.a1880337.png"
+  },
+  {
+    "revision": "7b502e3dd0e2ff24dd5d8403da208100",
+    "url": "/static/media/workshop-audience.7b502e3d.jpg"
+  },
+  {
+    "revision": "a21873da87754f5f51289a757a004577",
+    "url": "/static/media/workshop-dev-tools.a21873da.jpg"
+  },
+  {
+    "revision": "4f42ac29aaa970a0d65257abddcccba7",
+    "url": "/static/media/workshop-kaylie.4f42ac29.jpg"
+  },
+  {
+    "revision": "6c06050e4f603b71f18c5ea0e1f5b1b9",
+    "url": "/static/media/workshop-scratch.6c06050e.jpg"
+  },
+  {
+    "revision": "0c97847a8fa8d7dcb7edb4c17d2b2fbb",
+    "url": "/static/media/yubico.0c97847a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.7381e95567de53f1b8848a29f73bf7c1.js"
+  "/precache-manifest.2a693c4bf7508cdd5dd8350b7b6d316f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
