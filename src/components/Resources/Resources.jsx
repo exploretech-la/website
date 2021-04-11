510b9dfbf8c931174c3d5bbe66f11e9fd9bc1162
Vivian Doan <doanpvivian@gmail.com>
2021-04-11 14:37:52 -0700

    Add Ceremonies component with dummy embedded videos

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Ceremonies from './Ceremonies';
 import Footer from '../Footer';
 
 function Resources() {
@@ -8,9 +9,10 @@ function Resources() {
       <div className="resources-title">
           <h1>Resources</h1>
           <div className="pill-divider" />
-          <div className="resources-text">
+          <div className="Section resources-text">
             <h5>Come back after our event to access resources and recordings of all workshops and panels!</h5>
           </div>
+          <Ceremonies />
       </div>
       <Footer />
     </div>
