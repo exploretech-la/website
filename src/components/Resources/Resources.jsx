2348133a7bf1b31f434735b112ed07379e28b546
Kyle Icban <kicban29@gmail.com>
2022-03-31 23:00:31 -0700

    new changes lol

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -18,7 +18,27 @@ function Resources() {
               resources for the activity.
             </h5>
           </div>
-          <Ceremonies />
+          <div className='Section event-program-button'>
+            <Button size="lg" variant="outline-info" href={EventProgram} target="_blank">
+              Click here to view the full event program
+            </Button>
+          </div>
+          <Schedule />
+          <Maps maps={maps} />
+          <div className='Section resources-title'>
+            <h2>Wristbands</h2>
+            <div className="pill-divider" />
+            <h5><b>Make sure you have your wristband on the entire time!</b></h5>
+            <div className='resources-text'>
+              <h5>
+                All high school students are divided into 3 groups, each will get a different colored wristband. 
+                During each session, 1 group will be at the exhibition hall and the other 2 groups will be at the workshops. 
+                Group 1 (green colored-wristband)  will be at the exhibition hall during Session 1, 
+                Group 2 (blue) during Session 2, and Group 3 (red) during Session 3.
+              </h5>
+            </div>
+            <img style={{marginTop: '50px'}} src={wristbands} alt="Wristband coordination" />
+          </div>
           <Workshops />
       </div>
       <Footer />
