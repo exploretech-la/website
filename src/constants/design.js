af3e0bbc45a705911b22ff5f7b9232485dea23cd
Matthew Ryan Teo <matthewrynt@gmail.com>
2023-01-18 15:52:36 -0800

    design-updates

--- src/constants/design.js
+++ src/constants/design.js
@@ -2,32 +2,32 @@ export const Design = [
     {
         key: 1,
         name: 'Belle Lerdworatawee',
-        image: require('static/team/design/andy-shen.png'),
+        image: require('static/team/design/belle-lerdworatawee.jpg'),
         link: '',
     }, {
         key: 2,
         name: 'David Xiong',
-        image: require('static/team/design/joyce-mok.png'),
+        image: require('static/team/design/david-xiong.jpg'),
         link: '',
     }, {
         key: 3,
         name: 'Deanna Gee',
-        image: require('static/team/design/kyle-icban.png'),
+        image: require('static/team/design/deanna-gee.jpg'),
         link: '',
     }, {
         key: 4,
         name: 'Emily Poon',
-        image: require('static/team/design/raphael-gatchalian.png'),
+        image: require('static/team/design/emily-poon.jpg'),
         link: '',
     }, {
         key: 5,
         name: 'Raphael Gatchalian',
-        image: require('static/team/design/vy-truong.png'),
+        image: require('static/team/design/raphael-gatchalian.jpg'),
         link: '',
     }, {
         key: 6,
         name: 'Samantha Chou',
-        image: require('static/team/design/vy-truong.png'),
+        image: require('static/team/design/samantha-chou.jpg'),
         link: '',
     },
 ];
